b8feddb4c630b2e530f49a15086b190fa2ba16b1
Jibble330 <jettb6@gmail.com>
2023-01-23 08:01:08 -0800

    Update name

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ README = (this_directory / "README.md").read_text()
 setup(
     name='DynamicHtml',
     packages=find_packages(exclude=("tests")),
-    version='1.0.2',
+    version='1.0.3',
     description='An extremely simple library used for gathering html from dynamic webpages using Pyppeteer.',
-    url='https://github.com/Jibble330/DynamicHtml',
-    author='Jibble',
+    url='https://github.com/commonkestrel/DynamicHtml',
+    author='Kestrel',
     license='MIT',
     install_requires=['pyppeteer'],
     setup_requires=['pytest-runner'],
